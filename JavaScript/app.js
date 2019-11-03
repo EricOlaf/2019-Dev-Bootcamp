e7ffcbafa2bd3e7e33fc9b1a933bf13748298075
Eric Olaveson <olavesoneric@gmail.com>
2019-11-03 12:12:12 -0600

    question

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,4 +1,12 @@
-var x = 3;
-var y = ++x;
-y += 1;
-console.log(y)
+// var x = 3;
+// var y = ++x;
+// y += 1;
+// console.log(y)
+// // y = 5
+
+// var x = 3;
+// var y = x++;
+// y += 1;
+// console.log(y)
+// // y = 4
+
